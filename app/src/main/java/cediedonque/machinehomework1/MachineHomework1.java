33d68346a3688de5ce4a1631c983b500a412cf7a
Cedie Donque <ceeedang@gmail.com>
2018-07-27 13:22:59 +0800

    This is my Machine Homework 1

---
+++ app/src/main/java/cediedonque/machinehomework1/MachineHomework1.java
@@ -0,0 +1,13 @@
+package cediedonque.machinehomework1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MachineHomework1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_machine_homework1);
+    }
+}
